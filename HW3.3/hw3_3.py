84f000187d1b5dc680de08d0e5817d0e1dd1b19a
AleksandraMomoka <tishchenkoaleksandra2002@gmail.com>
2025-01-26 12:23:55 +0200

    HW3.3

--- HW3.3/hw3_3.py
+++ HW3.3/hw3_3.py
@@ -1,12 +1,14 @@
 #lst = [1, 2, 3, 4, 5, 6] #=> [[1, 2, 3], [4, 5, 6]]
-#lst = [1, 2, 3] #=> [[1, 2], [3]]
+lst = [1, 2, 3] #=> [[1, 2], [3]]
 #lst = [1, 2, 3, 4, 5] #=> [[1, 2, 3], [4, 5]]
 #lst = [1] #=> [[1], []]
-lst = [] #=> [[], []]
+#lst = [] #=> [[], []]
 
 lst_mid = (len(lst) + 1) // 2
 
 lst_first_part = lst[:lst_mid]
 lst_second_part = lst[lst_mid:]
 
-print([lst_first_part, lst_second_part])
+lst_result = [lst_first_part, lst_second_part]
+
+print(lst_result)
